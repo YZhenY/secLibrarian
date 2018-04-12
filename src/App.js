148e8536a7408d7d22c062e44cedefccd13e3a0b
YZhenY <fuz3zy@gmail.com>
2018-04-12 12:11:12 +0800

    added semantic-ui to build on

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Segment} from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: ''
+    }
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(e) {
+    var url = "https://sec.gov/";
+    fetch(url, {
+      headers: {
+      },
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, cors, *same-origin
+      redirect: 'follow', // *manual, follow, error
+      referrer: 'no-referrer', // *client, no-referrer
+    })
+    .then(response => response.text())
+    .then( result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button onClick={this.handleSearch}>Test</Button>
+        <Segment>Stuff here: {this.state.result} </Segment>
       </div>
     );
   }
