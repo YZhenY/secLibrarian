2abbc72a9f8e209fb30a56ce6fc9611b459b8125
YZhenY <fuz3zy@gmail.com>
2018-04-12 14:01:06 +0800

    commit before ejecting create react app

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
+console.log(__dirname + '/../build');
+app.use(express.static(__dirname + '/../build'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
